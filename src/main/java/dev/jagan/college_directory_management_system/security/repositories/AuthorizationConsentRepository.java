76969a2b0a7d414ca388cbba7c97fad856be321a
Jagan Baskar <jackjagan1995@gmail.com>
2024-09-14 20:47:08 +0530

    Added OAuth2

---
+++ src/main/java/dev/jagan/college_directory_management_system/security/repositories/AuthorizationConsentRepository.java
@@ -0,0 +1,13 @@
+package dev.jagan.college_directory_management_system.security.repositories;
+
+import dev.jagan.college_directory_management_system.security.models.AuthorizationConsent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorizationConsentRepository extends JpaRepository<AuthorizationConsent, AuthorizationConsent.AuthorizationConsentId> {
+    Optional<AuthorizationConsent> findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+}
---
+++ src/main/java/dev/jagan/college_directory_management_system/security/repositories/ClientRepository.java
@@ -0,0 +1,12 @@
+package dev.jagan.college_directory_management_system.security.repositories;
+
+import dev.jagan.college_directory_management_system.security.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
